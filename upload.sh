65664d811757a8b95ae75467669205f7d6d93f9e
François Kooman <fkooman@tuxed.net>
2020-07-08 12:58:04 +0200

    fix upload.sh

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf output
 php bin/generate.php
-rsync -avzuh -e ssh output/docs/* argon.tuxed.net:/var/www/html/fkooman/docs.eduvpn.org --progress --exclude '.git'
+rsync -avzuh -e ssh output/* argon.tuxed.net:/var/www/html/fkooman/docs.eduvpn.org --progress --exclude '.git'
